971a8c25f36a0e627f933d57026dd48a947a05f8
Thiago Araujo <thiago.araujo72@etec.sp.gov.br>
2022-06-29 20:40:46 -0300

    ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,12 +2,25 @@ const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
 const makeSystemUnderTest = () => {
-  return new LoginRouter()
+  class AuthUseCaseSpy {
+    auth (email, password) {
+      this.email = email
+      this.password = password
+    }
+  }
+
+  const authUseCaseSpy = new AuthUseCaseSpy()
+  const systemUnderTest = new LoginRouter(authUseCaseSpy)
+
+  return {
+    systemUnderTest,
+    authUseCaseSpy
+  }
 }
 
 describe('Login Router', () => {
   test('Should return 400 if no email is provided', () => {
-    const systemUnderTest = makeSystemUnderTest()
+    const { systemUnderTest } = makeSystemUnderTest()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -21,7 +34,7 @@ describe('Login Router', () => {
   })
 
   test('Should return 400 if no password is provided', () => {
-    const systemUnderTest = makeSystemUnderTest()
+    const { systemUnderTest } = makeSystemUnderTest()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com'
@@ -34,16 +47,31 @@ describe('Login Router', () => {
   })
 
   test('Should return 500 if no httpRequest is provided', () => {
-    const systemUnderTest = makeSystemUnderTest()
+    const { systemUnderTest } = makeSystemUnderTest()
     const httpResponse = systemUnderTest.route()
 
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('Should return 500 if no httpRequest has no body', () => {
-    const systemUnderTest = makeSystemUnderTest()
+    const { systemUnderTest } = makeSystemUnderTest()
     const httpResponse = systemUnderTest.route({ })
 
     expect(httpResponse.statusCode).toBe(500)
+  })
+})
+
+describe('Login Router Integration AuthCase', () => {
+  test('Should call AuthUseCase with corret param', () => {
+    const { systemUnderTest, authUseCaseSpy } = makeSystemUnderTest()
+    const httpRequest = {
+      body: {
+        email: 'any-email@mail.com',
+        password: 'any_password'
+      }
+    }
+    systemUnderTest.route(httpRequest)
+    expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
+    expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
   })
 })
