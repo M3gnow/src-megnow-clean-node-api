46144f0cff83e4fb98af2f0daf2b95177bcae664
Thiago Araujo <thiago.araujo72@etec.sp.gov.br>
2022-06-29 20:01:24 -0300

    ensure LoginRouter returns 500 if an invalid httpRequest is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,11 @@
 class LoginRouter {
   route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
+
     const { email, password } = httpRequest.body
 
     if (!email || !password) {
@@ -23,9 +29,7 @@ describe('Login Router', () => {
 
     expect(httpResponse.statusCode).toBe(400)
   })
-})
 
-describe('Login Router', () => {
   test('Should return 400 if no password is provided', () => {
     const systemUnderTest = new LoginRouter()
     const httpRequest = {
@@ -37,5 +41,19 @@ describe('Login Router', () => {
     const httpResponse = systemUnderTest.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 500 if no httpRequest is provided', () => {
+    const systemUnderTest = new LoginRouter()
+    const httpResponse = systemUnderTest.route()
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if no httpRequest has no body', () => {
+    const systemUnderTest = new LoginRouter()
+    const httpResponse = systemUnderTest.route({ })
+
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
