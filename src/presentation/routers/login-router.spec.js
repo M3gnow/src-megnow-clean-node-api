8c81286b9595972afaa2c66472352031ce7cbaef
Thiago Araujo <thiago.araujo72@etec.sp.gov.br>
2022-06-29 19:51:03 -0300

    ensure LoginRouter return 400 if no password is not provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,21 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+
+    const httpResponse = systemUnderTest.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
+
+describe('Login Router', () => {
+  test('Should return 400 if no password is provided', () => {
+    const systemUnderTest = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
       }
     }
 
