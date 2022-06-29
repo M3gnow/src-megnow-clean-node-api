864700605f0fe1e84433a952ff866f3073c3d9f1
Thiago Araujo <thiago.araujo72@etec.sp.gov.br>
2022-06-29 19:46:50 -0300

    emprove LoginRoute statusCode 400 if email not provided

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,24 @@
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const systemUnderTest = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = systemUnderTest.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
